e76eae4e9ea18000239bbc0c2f4862f80eda0a4e
yural64 <yural64@gmail.com>
2024-10-25 22:56:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,30 @@ import time
 def initialize_browser():
     # Инициализация драйвера (проверьте путь к драйверу, например, 'chromedriver.exe' для Windows)
     driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(5)
     return driver
 
 def search_wikipedia(driver, query):
-    driver.get("https://wikipedia.org")
+    driver.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
     search_box = driver.find_element(By.NAME, "search")
     search_box.send_keys(query)
     search_box.send_keys(Keys.RETURN)
 
+
 def list_paragraphs(driver):
     paragraphs = driver.find_elements(By.CSS_SELECTOR, "p")
-    for index, paragraph in enumerate(paragraphs):
-        print(f"\n--- Параграф {index + 1} ---\n{paragraph.text}")
-        if index % 3 == 2:  # Предлагать дальнейшие действия после каждых 3 параграфов
-            action = input("Введите 'n' для продолжения или 'q' для выхода в меню: ")
-            if action.lower() == 'q':
-                break
+    paragraph_counter = 0
+
+    for paragraph in paragraphs:
+        paragraph_text = paragraph.text.strip()  # Убираем лишние пробелы и переносы строк
+        if paragraph_text:  # Если текст параграфа не пустой
+            paragraph_counter += 1
+            print(f"\n--- Параграф {paragraph_counter} ---\n{paragraph_text}")
+
+            if paragraph_counter % 3 == 0:  # Предлагать дальнейшие действия после каждых 3 параграфов
+                action = input("Введите 'n' для продолжения или 'q' для выхода в меню: ")
+                if action.lower() == 'q':
+                    break
 
 def list_related_links(driver):
     links = driver.find_elements(By.CSS_SELECTOR, "#bodyContent a")
